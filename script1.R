aa37593691bbcff1bcf4693c1f824f14455b9f53
Rachael <rachaellatimer@gmail.com>
2021-02-20 20:50:41 -0800

    initial plots and draft of final plots
    includes highlighted countries: USA, Canada, Japan, average of all ("all")

--- script1.R
+++ script1.R
@@ -5,6 +5,7 @@ library(ggplot2)
 library(here)
 library(maps)
 library(dplyr)
+library(gghighlight)
 
 initial <- import(here("data", "food_carbon_footprint_data.xlsx")) %>% 
   clean_names() %>%
@@ -14,21 +15,21 @@ initial <- import(here("data", "food_carbon_footprint_data.xlsx")) %>%
 #filter out the top ten highest CO2/person/year
 subset <- initial %>% 
   mutate(ranking = as.numeric(ranking)) %>% 
-  filter(ranking < 11 |country == "all"| country =="Canada"|country ==)
+  filter(ranking < 11 |country == "all"| country =="Canada"|country =="Japan")
 
 #line plot work: CO2/person/year produced by country
 
-initial_longer <- topten %>%
+initial_longer <- subset %>%
   pivot_longer(cols = 3:16,
                names_to = "product",
                values_to = "CO2_person_year")
 
-animal <- topten %>%
+animal <- subset %>%
   pivot_longer(cols = 3:9,
                names_to = "product",
                values_to = "CO2_person_year")
 
-non_animal <- topten %>%
+non_animal <- subset %>%
   pivot_longer(cols = 11:14,
                names_to = "product",
                values_to = "CO2_person_year")
@@ -37,19 +38,48 @@ non_animal <- topten %>%
   #plot1: animal products
 #draft
 
-d1 <- animal %>% 
+a1 <- animal %>% 
   ggplot(aes(product, CO2_person_year, group=country)) +
   geom_line(aes(color = country))
-
-d1
-
+a1
 
 #final
+a2 <- animal %>% 
+  ggplot(aes(product, CO2_person_year, group=country)) +
+  geom_line(aes(color = country), size = 2) +
+  gghighlight(country == "all" |country == "USA" | country =="Canada"| country =="Japan", 
+              unhighlighted_params = list(size = 1)) +
+  scale_color_viridis_d() +
+  scale_x_discrete(expand = c(0, 0)) +
+  labs(title = "CO2/person/year for animal products",
+       subtitle = "",
+       x = "animal product",
+       y = "Co2/person/year (in 10,000lbs)") +
+  theme_minimal()
+a2
+
 
   #plot2: non-animal products
 #draft
+na1 <- non_animal %>% 
+  ggplot(aes(product, CO2_person_year, group=country)) +
+  geom_line(aes(color = country))
+na1
 
 #final
+na2 <- non_animal %>% 
+  ggplot(aes(product, CO2_person_year, group=country)) +
+  geom_line(aes(color = country), size = 2) +
+  gghighlight(country == "all" |country == "USA" | country =="Canada"| country =="Japan", 
+              unhighlighted_params = list(size = 1)) +
+  scale_color_viridis_d() +
+  scale_x_discrete(expand = c(0, 0)) +
+  labs(title = "CO2/person/year for non-animal products",
+       subtitle = "",
+       x = "non-animal product",
+       y = "Co2/person/year (in 10,000lbs)") +
+  theme_minimal()
+na2
 
   #plot3: difference between animal and non-animal products
 #draft
